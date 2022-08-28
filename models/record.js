18cbf49748c8aae429e0d64d71381f38d0ddda3a
youjheng <chagam0507@gmail.com>
2022-08-28 10:19:10 +0800

    feat: add Record model

---
+++ models/record.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const recordSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    required: true
+  },
+  amount: {
+    type: Number,
+    required: true
+  }
+})
+
+module.exports = mongoose.model('Record', recordSchema)
