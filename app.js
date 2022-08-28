774e0a0d2aa5e906fda3d3b96b34ff51b6f8ce05
youjheng <chagam0507@gmail.com>
2022-08-28 11:32:17 +0800

    feat: add bootstrap & modify main.hbs

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express')
 const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
 
+const Record = require('./models/record')
+
 const app = express()
 const port = 3000
 
@@ -21,6 +23,10 @@ app.set('view engine', 'hbs')
 
 app.get('/', (req, res) => {
   res.render('index')
+  Record.find()
+    .lean()
+    .then(records => res.render('index', { records }))
+    .catch(err => console.error(err))
 })
 
 app.listen(port, () => {
