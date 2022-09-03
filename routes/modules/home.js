29511f1f5528b4696dfeecbd36bc88e8e0c26b56
youjheng <chagam0507@gmail.com>
2022-09-03 19:01:50 +0800

    feat: add category icon

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -2,16 +2,23 @@ const express = require('express')
 const router = express.Router()
 
 const Record = require('../../models/record')
+const Category = require('../../models/category')
 
 router.get('/', (req, res) => {
   const userId = req.user._id
-  Record.find({ userId })
+  Category.find()
     .lean()
-    .then(records => {
-      records.forEach(record => record.date = record.date.toISOString().split('T')[0])
-      res.render('index', { records })
+    .then(() => {
+      Record.find({ userId })
+        .populate('category', 'icon')
+        .lean()
+        .then(records => {
+          records.forEach(record => record.date = record.date.toISOString().split('T')[0])
+          records.forEach(record => record.category = record.category.icon)
+          res.render('index', { records })
+        })
+        .catch(err => console.error(err))
     })
-    .catch(err => console.error(err))
 })
 
 module.exports = router
