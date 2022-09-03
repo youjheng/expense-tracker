ca0d8dfe8401f6a9c34a12f17fb5575b1a27ef46
youjheng <chagam0507@gmail.com>
2022-09-03 16:28:30 +0800

    modify categorySeeder and recordSeeder

--- models/seeds/categorySeeder.js
+++ models/seeds/categorySeeder.js
@@ -1,11 +1,16 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 const Category = require('../category')
 const categoryData = require('./categoryData.json').results
 const db = require('../../config/mongoose')
 
 db.once('open', () => {
-  console.log('mongodb connected!')
-  categoryData.map(data => {
-    Category.create(data)
-  })
-  console.log('done')
+  Promise.all(categoryData.map(data => {
+    return Category.create(data)
+  }))
+    .then(() => {
+      console.log('categorySeeder is done')
+      process.exit()
+    })
 })
--- models/seeds/recordSeeder.js
+++ models/seeds/recordSeeder.js
@@ -1,11 +1,41 @@
+const bcrypt = require('bcryptjs')
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 const Record = require('../record')
+const User = require('../user')
+const Category = require('../category')
 const recordData = require('./recordData.json').results
 const db = require('../../config/mongoose')
 
+const SEED_USER = {
+  name: 'Daddy',
+  email: 'daddy@example.com',
+  password: '12345678'
+}
+
 db.once('open', () => {
-  console.log('mongodb connected!')
-  recordData.map(seed => {
-    Record.create(seed)
-  })
-  console.log('done')
+  bcrypt
+    .genSalt(10)
+    .then(salt => bcrypt.hash(SEED_USER.password, salt))
+    .then(hash => User.create({
+      name: SEED_USER.name,
+      email: SEED_USER.email,
+      password: hash
+    }))
+    .then(user => {
+      return Promise.all(recordData.map(data => {
+        return Category.findOne({ name: data.category })
+          .lean()
+          .then(category => {
+            data.category = category._id
+            data.userId = user._id
+            return Record.create(data)
+          })
+      }))
+    })
+    .then(() => {
+      console.log('recordSeeder is done')
+      process.exit()
+    })
 })
